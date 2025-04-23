7ad6ab9d20f70f48ce330d72e528cc3e9f1c982f
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-23 13:39:14 +0400

    Added a sliding drawer menu for the navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  private menuOpen = signal(false);
 
+  isMenuOpen = () => this.menuOpen();
+  toggleMenu = () => this.menuOpen.set(!this.menuOpen());
+  closeMenu = () => this.menuOpen.set(false);
 }
