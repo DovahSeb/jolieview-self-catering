aed39ac287e64818b60378fed3e2e8e17a47dfc7
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-25 13:27:53 +0400

    Updated the contact form to send email

--- src/app/features/components/contact/contact.component.ts
+++ src/app/features/components/contact/contact.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
+interface ContactForm{
+  name: string;
+  email: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  email = 'contact@jolieviewseychelles.com'
+  contactForm: ContactForm = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  contactEmail = 'contact@jolieviewseychelles.com'
+  showSuccess = false;
+  showValidation = false;
+
+  sendEmail() {
+    const { name, email, message } = this.contactForm;
+
+    if (!name && !email && !message) {
+      this.showValidation = true;
+      setTimeout(() => {
+        this.showValidation = false;
+      }, 2000);
+      return;
+    }
+
+    emailjs
+    .send('service_zx93o68', 'template_4sgse0k', { ...this.contactForm }, {
+        publicKey: '-rugXXE4tnGPhX6jj',
+    })
+    .then(() => {
+      this.showSuccess = true;
+      this.contactForm = {
+        name: '',
+        email: '',
+        message: '',
+      };
+      setTimeout(() => {
+        this.showSuccess = false;
+      }, 3000);
+    },
+      (error: EmailJSResponseStatus) => {
+        this.showValidation = true;
+        setTimeout(() => {
+          this.showValidation = false;
+        }, 2000);
+        console.log('FAILED!', error.text);
+      }
+    );
+  }
 }
