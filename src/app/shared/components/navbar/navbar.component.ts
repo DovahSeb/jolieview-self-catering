ddcfe5805c8ef888605e7ee1d4837018879f15e0
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-28 11:21:27 +0400

    Added a logo and animation to navbar toggle icon

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  faBars = faBars;
+  faXmark = faXmark;
+
   private menuOpen = signal(false);
 
   isMenuOpen = () => this.menuOpen();
