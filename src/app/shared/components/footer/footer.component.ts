18047a30d0dd8ca80c414049ac9da8188652e836
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-28 10:14:31 +0400

    Added icons next to contact information

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  faLocationDot = faLocationDot;
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+
   email = 'contact@jolieviewseychelles.com'
   year = new Date().getFullYear();
 }
